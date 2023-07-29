1e9cc22e09dbab917e7838a2c7126d5b797769a2
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-07-29 19:49:51 +0530

    feat: handle delete playlist

--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -1,25 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import { useVideos } from "../main";
+import CloseIcon from "@mui/icons-material/Close";
+import { videoConstants } from "../constants/video-constants";
 
 export const Playlists = () => {
   const navigate = useNavigate();
   const {
     videos: { playlists },
+    setVideos,
   } = useVideos();
+
+  const { DELETE_PLAYLIST } = videoConstants;
 
   return (
     <div className="flex flex-col gap-5 px-5 md:w-5/6">
       <div className="text-2xl font-bold">Playlists</div>
       <div className="flex flex-wrap gap-10">
-        {playlists?.map((item) => {
-          const { _id, title, description, videos } = item;
+        {playlists?.map((playlist) => {
+          const { _id, title, description, videos } = playlist;
           return (
             <div
               key={_id}
-              className="flex flex-col gap-2 hover:cursor-pointer hover:text-blue-400"
+              className="relative flex flex-col gap-2 hover:cursor-pointer hover:text-blue-400"
               rel="noreferrer"
               onClick={() => navigate(`/playlist/${_id}`)}
             >
+              <div
+                className="absolute right-0 top-0 rounded-bl-md bg-white p-1 hover:cursor-pointer"
+                title="Delete Playlist"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setVideos({ type: DELETE_PLAYLIST, payload: playlist });
+                }}
+              >
+                <CloseIcon />
+              </div>
               <div>
                 <img src="https://picsum.photos/320/176" />
               </div>
--- src/reducers/videos-reducer.jsx
+++ src/reducers/videos-reducer.jsx
@@ -7,6 +7,7 @@ export const videosReducer = (state, { type, payload }) => {
     SET_EXPLORE_SEARCH_TEXT,
     SET_SELECTED_PLAYLIST,
     REMOVE_FROM_PLAYLIST,
+    DELETE_PLAYLIST,
   } = videoConstants;
 
   switch (type) {
@@ -39,6 +40,11 @@ export const videosReducer = (state, { type, payload }) => {
             : playlist
         ),
       };
+    case DELETE_PLAYLIST:
+      return {
+        ...state,
+        playlists: state?.playlists?.filter(({ _id }) => _id != payload?._id),
+      };
     default:
       return state;
   }
@@ -62,6 +68,43 @@ export const initialVideos = {
           src: "https://www.youtube.com/embed/GBIIQ0kP15E",
           category: "Stop Motion",
           creator: "AnimateMagic",
+        },
+        {
+          _id: 36,
+          title: "Quilling 3D Wall Art - Adding Depth to Paper Designs",
+          views: 1756,
+          chips: ["quilling", "3d wall art", "paper", "designs"],
+          thumbnail: "https://picsum.photos/312/174",
+          src: "https://www.youtube.com/embed/GBIIQ0kP15E",
+          category: "Quilling",
+          creator: "PaperArtDimensions",
+        },
+      ],
+    },
+    {
+      _id: 4,
+      title: "Demo 2",
+      description: "Desc 2",
+      videos: [
+        {
+          _id: 35,
+          title: "Pottery Art Exhibition - Celebrating Local Artists",
+          views: 2879,
+          chips: ["pottery", "clay", "art exhibition", "local artists"],
+          thumbnail: "https://picsum.photos/311/174",
+          src: "https://www.youtube.com/embed/GBIIQ0kP15E",
+          category: "Pottery",
+          creator: "ArtisticExpressions",
+        },
+        {
+          _id: 36,
+          title: "Quilling 3D Wall Art - Adding Depth to Paper Designs",
+          views: 1756,
+          chips: ["quilling", "3d wall art", "paper", "designs"],
+          thumbnail: "https://picsum.photos/312/174",
+          src: "https://www.youtube.com/embed/GBIIQ0kP15E",
+          category: "Quilling",
+          creator: "PaperArtDimensions",
         },
       ],
     },
