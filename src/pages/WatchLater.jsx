a74906c2d6d6dba375fafc60b19b8db705cb6fe0
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-07-29 16:02:31 +0530

    feat: add watch later page

--- src/pages/WatchLater.jsx
+++ src/pages/WatchLater.jsx
@@ -1,3 +1,21 @@
+import { VideoCard } from "../components/VideoCard";
+import { useVideos } from "../main";
+
 export const WatchLater = () => {
-  return <div>Watch Later</div>;
+  const {
+    videos: { allVideos },
+  } = useVideos();
+
+  const watchLaterVideos = allVideos?.filter(({ watchLater }) => watchLater);
+
+  return (
+    <div className="flex flex-col gap-5 px-5">
+      <div className="text-2xl font-bold">Watch Later</div>
+      <div className="flex flex-wrap gap-5">
+        {watchLaterVideos?.map((video) => (
+          <VideoCard video={video} key={video._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
